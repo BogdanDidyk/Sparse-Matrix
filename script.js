17d32fbb16c4b70a4fe8c415f45c323bd9c5635c
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-04-07 22:15:38 +0300

    Added the function that returns the number of non zeros matrix items

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function printMatrix(matrix) {
     matrix.forEach(row => console.log(row.join(" ")));
 }
+
+function getNumberOfNonZerosMatrixItems(sparseMatrix) {
+    const rows = sparseMatrix.length;
+    const cols = sparseMatrix[0].length;
+    let nonZerosItems = 0;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (sparseMatrix[i][j] != 0) nonZerosItems++;
+        }
+    }
+
+    return nonZerosItems;
+}
