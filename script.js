d12649f9d034a6329ba34dcc65fa415dc7839976
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-04-08 23:27:53 +0300

    Added the function that compress the sparse matrix

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ function IsPossibleToSaveMemoryForMatrix(matrix) {
 
     return (3 * nonZerosItems) < (rows * cols); 
 }
+
+function compressSparseMatrix(sparseMatrix) {
+    if (!IsPossibleToSaveMemoryForMatrix(sparseMatrix)) return sparseMatrix;
+
+    const rows = sparseMatrix.length;
+    const cols = sparseMatrix[0].length;
+    const rowIndices = [];
+    const colIndices = [];
+    const values = [];
+    let index = 0;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (sparseMatrix[i][j] != 0) {
+                rowIndices[index] = i;
+                colIndices[index] = j;
+                values[index] = sparseMatrix[i][j];
+                index++;
+            }
+        }
+    }
+
+    return [rowIndices, colIndices, values];
+}
